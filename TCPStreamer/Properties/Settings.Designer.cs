94f64f1dfd03d0d9095dc22d188d42f1d323212e
Danny Bates <59747718+dbn5tm@users.noreply.github.com>
2023-03-03 09:10:01 -0600

    1.0.0.15 Version 1.0.4

--- TCPStreamer/Properties/Settings.Designer.cs
+++ TCPStreamer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TCPStreamer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -52,7 +52,7 @@ namespace TCPStreamer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("192.168.0.98")]
+        [global::System.Configuration.DefaultSettingValueAttribute("192.168.1.9")]
         public string StartUpIP {
             get {
                 return ((string)(this["StartUpIP"]));
